9ccae7cda4a62ecf1801e793b36e3345afa8aa86
mounika-nalla <mounika.nalla@ibm.com>
2023-06-14 12:21:01 -0400

    feat: update references, version

--- examples/sg-target-example/main.tf
+++ examples/sg-target-example/main.tf
@@ -21,7 +21,8 @@ data "ibm_resource_group" "existing_resource_group" {
 
 module "vpc" {
   count             = var.vpc_id != null ? 0 : 1
-  source            = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=v6.0.1"
+  source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
+  version           = "7.3.0"
   resource_group_id = var.resource_group != null ? data.ibm_resource_group.existing_resource_group[0].id : ibm_resource_group.resource_group[0].id
   region            = var.region
   prefix            = var.prefix
