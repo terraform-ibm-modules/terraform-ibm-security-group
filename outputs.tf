a8d147217ee92d959d149c929d56a15757df4560
jvallexm <jvallexm@gmail.com>
2023-08-23 14:19:30 -0400

    fix: outputs

--- outputs.tf
+++ outputs.tf
@@ -11,12 +11,12 @@ output "security_group_id" {
   description = "The ID of the security group where the rules are added"
   value       = local.sg_id
 
-  depends_on = [ibm_is_security_group_rule.rule]
+  depends_on = [ibm_is_security_group_rule.security_group_rule]
 }
 
 output "security_group_rule" {
   description = "Security group rules"
-  value       = ibm_is_security_group_rule.rule
+  value       = ibm_is_security_group_rule.security_group_rule
 }
 
 ##############################################################################
