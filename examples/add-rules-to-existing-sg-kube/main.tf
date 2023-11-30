1264bc085b6fe31449c1dabd22995a1040a01f71
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2023-11-30 12:18:28 +0000

    feat: add example manupulating kube sgs

---
+++ examples/add-rules-to-existing-sg-kube/main.tf
@@ -0,0 +1,64 @@
+##############################################################################
+# Find VPC ID based on the inputed cluster_id
+##############################################################################
+
+locals {
+  vpc_id = data.ibm_container_vpc_worker_pool.worker_pool.vpc_id
+}
+
+data "ibm_container_vpc_cluster" "cluster" {
+  name = var.cluster_id
+}
+
+# No vpc_id output on ibm_container_vpc_cluster so using the ibm_container_vpc_worker_pool data (which does have a vpc_id)
+data "ibm_container_vpc_worker_pool" "worker_pool" {
+  cluster          = var.cluster_id
+  worker_pool_name = data.ibm_container_vpc_cluster.cluster.worker_pools[0].name
+}
+
+
+##############################################################################
+# Build name of the SGs
+# See https://cloud.ibm.com/docs/openshift?topic=openshift-vpc-security-group
+##############################################################################
+
+locals {
+  name_of_worker_nodes_sg = "kube-${var.cluster_id}"
+  name_of_lb_vpe_sg       = "kube-${local.vpc_id}"
+}
+
+##############################################################################
+# Add rules to existing worker node security group
+##############################################################################
+
+module "add_rules_to_workernodes_sg" {
+  source                       = "../.."
+  add_ibm_cloud_internal_rules = false # No need as handled by OpenShift stack
+  use_existing_security_group  = true
+  existing_security_group_name = local.name_of_worker_nodes_sg
+  security_group_rules = [{
+    name      = "allow-example-inbound"
+    direction = "inbound"
+    remote    = "192.0.2.0/24"
+  }]
+}
+
+##############################################################################
+# Add rules to existing lb/vpe security group
+##############################################################################
+
+module "add_rules_to_lbvpc_sg" {
+  source                       = "../.."
+  add_ibm_cloud_internal_rules = false # No need as handled by OpenShift stack
+  use_existing_security_group  = true
+  existing_security_group_name = local.name_of_lb_vpe_sg
+  security_group_rules = [{
+    name      = "allow-example-inbound"
+    direction = "inbound"
+    remote    = "192.0.2.0/24"
+    tcp = {
+      port_min = 443
+      port_max = 443
+    }
+  }]
+}
