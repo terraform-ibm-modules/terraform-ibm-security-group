c8db5a0f82336b818dcdbeddf2c780beaff7f275
Todd Giguere <todd.giguere@ibm.com>
2024-02-22 10:15:53 -0500

    fix: remove depends_on for outputs

--- outputs.tf
+++ outputs.tf
@@ -10,8 +10,6 @@ output "security_target" {
 output "security_group_id" {
   description = "The ID of the security group where the rules are added"
   value       = local.sg_id
-
-  depends_on = [ibm_is_security_group_rule.security_group_rule]
 }
 
 output "security_group_rule" {
