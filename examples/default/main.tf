1802b4677d6fa6e0f247925e918876cf92e747e9
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-05-12 09:12:52 +0000

    chore(deps): update terraform-module

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -5,7 +5,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.4"
+  version = "1.1.5"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -19,7 +19,7 @@ module "resource_group" {
 module "vpc" {
   count             = var.vpc_id != null ? 0 : 1
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.13.2"
+  version           = "7.18.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
